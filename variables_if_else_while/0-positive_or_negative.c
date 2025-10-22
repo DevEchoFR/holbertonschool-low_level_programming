23b75b7922c6f739b531eb7b1d038dab16188cef
Vesna David ROSET <snaboy18@gmail.com>
2025-10-22 09:11:48 +0000

    Fix: reupdate script for prints random n as positive, zero, or negative, ANSI C/Betty compliant.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
+
 int main(void)
 {
 	int n;
 	srand(time(0));
-	n = rand() - RAND_MAX / 2:
-		printf("%d\n", n);
-	return 0;
+	n = rand() - RAND_MAX / 2;
+	printf("%d\n", n);
+	return (0);
 }
