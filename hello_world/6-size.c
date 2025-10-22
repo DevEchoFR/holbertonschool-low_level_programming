5cbcd48b467f718943ba5f114336826e6158aab5
Vesna David ROSET <snaboy18@gmail.com>
2025-10-22 07:31:23 +0000

    Feat: writing C program for print the size of various

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void) {
+    printf("sizeof(int)       = %d\n",sizeof(int));
+    printf("sizeof(float)     = %d\n",sizeof(float));
+    printf("sizeof(double)    = %d\n",sizeof(double));
+    printf("sizeof(char)      = %d\n",sizeof(char));
+    printf("sizeof(long_int) = %d\n",sizeof(long_int));
+    printf("sizeof(long_long_int)       = %d\n",sizeof(long_long_int));
+    return 0;
+}
