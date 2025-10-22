ea7d6975c0f23589ffce7b02d083e4fe1c139f56
Vesna David ROSET <snaboy18@gmail.com>
2025-10-22 19:11:29 +0000

    Fix: reupdate the script to print the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the last digit of a random number and compares it
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n, lastDigit;
+    int n;
+    int lastDigit;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-lastDigit = n % 10;
-printf("%d ", n);
-if (lastDigit > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-}
-else if (lastDigit == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-}
-else if (lastDigit < 6 && lastDigit != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-}
-return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    lastDigit = n % 10;
+    printf("Last digit of %d is %d ", n, lastDigit);
+    if (lastDigit > 5)
+    {
+        printf("and is greater than 5\n");
+    }
+    else if (lastDigit == 0)
+    {
+        printf("and is 0\n");
+    }
+    else
+    {
+        printf("and is less than 6 and not 0\n");
+    }
+    return (0);
 }
