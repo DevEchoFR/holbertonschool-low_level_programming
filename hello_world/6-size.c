a7183bbd26f78c23686df1b20be044ceea0b6346
Vesna David ROSET <snaboy18@gmail.com>
2025-10-22 07:37:50 +0000

    Fix: update the script for print the size of various

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main (void) {
-    printf("sizeof(int)       = %d\n",sizeof(int));
-    printf("sizeof(float)     = %d\n",sizeof(float));
-    printf("sizeof(double)    = %d\n",sizeof(double));
-    printf("sizeof(char)      = %d\n",sizeof(char));
-    printf("sizeof(long_int) = %d\n",sizeof(long_int));
-    printf("sizeof(long_long_int)       = %d\n",sizeof(long_long_int));
-    return 0;
-}
+#include <stdio.h>                                                                                                                  
+                                                                                                                                       
+/**                                                                                                                                 
+* main - Entry point                                                                                                               
+* Return: Always 0 (Success)                                                                                                       
+*/                                                                                                                                 
+int main(void)                                                                                                                      
+{                                                                                                                                   
+      printf("Size of a char: %lu byte(s)\n", sizeof(char));                                                                      
+      printf("Size of an int: %lu byte(s)\n", sizeof(int));                                                                       
+      printf("Size of a long int: %lu byte(s)\n", sizeof(long int));                                                              
+      printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));                                                     
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));                                                                    
+       return (0);                                                                                                                 
+  }
