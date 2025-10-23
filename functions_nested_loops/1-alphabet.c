775340344fa9970e49d9ca0fd6b77a4bbab322df
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 19:17:14 +0000

    update for testing

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prototype void print_alphabet(void)
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void) 
+
+{
+
+for(char ch= 'a'; ch <= 'z'; ch++) 
+
+{
+
+putchar(ch) ;
+
+}
+
+}
+
+int main() 
+
+{
+
+print_alphabet() ;
+
+return 0;
+
+}
