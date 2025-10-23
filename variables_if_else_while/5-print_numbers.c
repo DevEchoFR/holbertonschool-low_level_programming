775d451a2e3cd6cac10f1e57c0f11b8b76725688
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 11:46:27 +0000

    update

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,9 +3,10 @@
  * main - Prints the lowercase alphabet followed by a newline
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 int i;
-for (i=0; i<=9; i++)
+for (i=0; i<=9; i++);
 printf("%d", i);
 printf("\n");
 return 0;
