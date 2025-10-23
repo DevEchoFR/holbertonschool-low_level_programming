a5b03d7db4a10aa0a55df0231c1736dcf16fff7e
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 14:14:08 +0000

    testing script

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet followed by a newline
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar (n + '0');
+putchar (',');
+}
+putchar ('\n');
+return (0);
+}
