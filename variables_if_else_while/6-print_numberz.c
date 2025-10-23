1680bf2063aa38b389a352b0215b02fb51261959
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 12:09:54 +0000

    Feat: Write a script focused on using putchar.

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 0; i <= 9; i++)
+putchar('0' + i);
+putchar('\n');
+return 0;
+}
