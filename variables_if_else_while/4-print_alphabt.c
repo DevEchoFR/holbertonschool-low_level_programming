9b450f79c606ece85c5637faba14f4b87909f2ce
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 09:43:34 +0000

    Feat: wirting script for print alphabet and specify some alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == 'q' || c == 'e')
+{
+continue;
+}
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
