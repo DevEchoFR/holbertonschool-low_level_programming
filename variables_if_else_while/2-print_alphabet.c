2f7bd1d4e2cdc22e235b20a835cb7924d95af865
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 09:18:13 +0000

    Feat: wirting script for print alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
