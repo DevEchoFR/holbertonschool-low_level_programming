372a7d7481485feeb481a491bc82a09a52b60baa
Vesna David ROSET <snaboy18@gmail.com>
2025-10-23 11:12:50 +0000

    Feat: wirting script for print single digit specify from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void){
+int i;
+for (i=1; i<=10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return 0;
+}
+
