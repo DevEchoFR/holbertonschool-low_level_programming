241b198a84b29a1181b155dfcf82cbbb47c512e0
Vesna David ROSET <snaboy18@gmail.com>
2025-10-24 18:57:29 +0000

    Fix: 1 if C is Aletter, 0 otherwise

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,39 +1,16 @@
 #include "main.h"
-
 /**
- * _isalpha - Checks if a character is alphabetic (lowercase or uppercase)
+ * _isalpha - entry point for check alphabetic character
+ * @c: thecharacter to check
+ * Return: 1 if uppercase, 0 if lowercase
  *
- * @c: The character to check, passed as an integer
- *
- * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-{
+if (c >= 65 && c <= 90)
 return (1);
-}
+else if (c >= 97 && c <= 122)
+return (1);
+else
 return (0);
 }
-
-/**
- * main - Entry point, tests _isalpha
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-int result;
-
-result = _isalpha('a');
-_putchar(result + '0');
-_putchar('\n');
-result = _isalpha('A');
-_putchar(result + '0');
-_putchar('\n');
-result = _isalpha('3');
-_putchar(result + '0');
-_putchar('\n');
-return (0);
-}
-
