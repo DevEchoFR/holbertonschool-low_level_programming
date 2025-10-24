d85ca775d721b92b67082f1e9c188aff9442c9af
Vesna David ROSET <snaboy18@gmail.com>
2025-10-24 18:00:05 +0000

    Fix: update script error in prototype print_alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,12 @@ void print_alphabet_x10(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
+int alphabet;
+alphabet = 'a';
+while (alphabet <= 'z')
 {
-_putchar(c);
+_putchar(alphabet);
+alphabet++;
 }
 _putchar('\n');
 }
