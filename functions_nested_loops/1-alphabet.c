23eb200c1fec3d2930faf5b1ae0fce9f7b2e0d38
Vesna David ROSET <snaboy18@gmail.com>
2025-10-24 17:32:30 +0000

    Fix: update script prototype print_alphabet for print alphabet lowercase

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,26 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a newline
+ * print_alphabet - Prints the alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
-int i;
+  int alphabet;
+  alphabet = 'a';
+  while (alphabet <= 'z')
+  {
+    _putchar(alphabet);
+    alphabet++;
+  }
+  _putchar('\n');
+}
 
-for (i = 97; i <= 122; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
-
-/**
- * main - Entry point, calls print_alphabet
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
