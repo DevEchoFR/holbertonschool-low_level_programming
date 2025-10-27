e164b0108c0fb8283f5699a90b360473eba16c59
RosetVesnaDavid <snaboy18@gmail.com>
2025-10-27 15:05:42 +0100

    Feat: write a function that multiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - entry point to check for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
