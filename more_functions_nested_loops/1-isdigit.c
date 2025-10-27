d4d768474c305d8ae5c120f937318baf9a7cdcc3
RosetVesnaDavid <snaboy18@gmail.com>
2025-10-27 14:50:28 +0100

    Feat: add _isdigit to checks for a digit (0 through 9).

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - entry point to check for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ *
+ */
+
+ int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
