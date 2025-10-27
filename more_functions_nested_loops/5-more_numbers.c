fc52c1a513c491e7cd7d3140c941e05e5ffbf384
RosetVesnaDavid <snaboy18@gmail.com>
2025-10-27 15:36:45 +0100

    Fix: script for print numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,16 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = '0'; i <= '15'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i * 10 + '0');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
