3c46872c8d1344e5cfd2f42c3986b6e755ca8965
Vesna David ROSET <snaboy18@gmail.com>
2025-10-29 12:42:31 +0000

    Test

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -11,6 +11,10 @@
 void _puts(char *str)
 {
 	int i = 0;
-    while (str[i] != '\0') i++;
-    return ('str');
+    while (str[i] != '\0')
+	{
+		_putchar ("str");
+		i++;
+	}
+    _putchar ("0");
 }
