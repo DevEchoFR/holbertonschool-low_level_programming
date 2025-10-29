c60be22a1b855f711b699c82af51a463ffac646a
Vesna David ROSET <snaboy18@gmail.com>
2025-10-29 13:03:14 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
-
-
+/**
+  *main - prints in reverse
+  *Return: 0
+*/
 
 void print_rev(char *s)
 {
+	int i = 0;
 	while (*s != '\0')
 	{
-		_putchar("str");
-		s++;
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }
