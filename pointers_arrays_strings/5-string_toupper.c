1b7f5c7c450e7dca89e9da2f61c6c80c9246480f
Vesna David ROSET <snaboy18@gmail.com>
2025-10-31 14:57:28 +0000

    feat(strings): add string_toupper (lower→upper)

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - converts all lowercase letters of a string to uppercase
+ * @s: pointer to the string to be converted
+ *
+ * Return: pointer to the modified string
+ */
+
+char *string_toupper(char *)
+{
+	char *ptr = s;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
+
+	return s;
+}
