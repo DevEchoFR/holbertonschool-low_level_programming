22388d497cfb50724730418fd92bcdbdd1eeb8bf
Vesna David ROSET <snaboy18@gmail.com>
2025-11-02 19:00:07 +0000

    Testing

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,27 +10,30 @@
 
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int result, sign, found_digit = 0, max, min;
 
-	// Skip whitespace characters
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-		   s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
-		i++;
+	result = 0;
+	sign = 1;
+	max = 2147483647;
+	min = -2147483648;
 
-	// Handle signs
-	while (s[i] == '+' || s[i] == '-')
+	while (*s)
 	{
-		if (s[i] == '-')
+		if (*s == '-')
 			sign *= -1;
-		i++;
-	}
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (*s - '0');
 
-	// Convert digits to integer
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+			if (sign == 1 && result > max)
+				return (max);
+			else if (sign == -1 && -result < min)
+				return (min);
+		}
+		else if (found_digit)
+			break;
 
-	return (sign * result);
+		s++;
+	}
 }
