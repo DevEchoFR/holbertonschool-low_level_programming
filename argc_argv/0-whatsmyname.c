00dc1453aac8726e0ff1e2eff05113ac2fa34efc
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 18:47:12 +0000

    feat(cli): print program name.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the program name, followed by a new line
+ * @argc: number of command-line arguments
+ * @argv: array of command-line argument strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
