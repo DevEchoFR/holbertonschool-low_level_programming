96536d45f4949997e8d6f18185ecc131c2e3dba4
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 13:26:06 +0000

    feat(math): add _pow_recursion (x^y recursive).

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the base number
+ * @y: the exponent number
+ *
+ * Return: the result of x raised to the power of y
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
