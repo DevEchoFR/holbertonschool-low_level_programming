55aa9ab20b4420799aafcc0459cd103c360c71f9
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 15:04:37 +0000

    feat(math): add is_prime_number + helper.

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * is_prime_helper - helper function to determine if n is prime
+ * prime - helper function to determine if n is prime
  * @n: number to check
  * @i: iterator
  *
  * Return: 1 if n is prime, 0 otherwise
  */
 
-is_prime_helper(int n, int i)
+int prime(int n, int i)
 {
 	if (i * i > n)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return (is_prime_helper(n, i + 1));
+	return (prime(n, i + 1));
 }
 
 /**
@@ -29,5 +29,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (is_prime_helper(n, 2));
+	return (prime(n, 2));
 }
