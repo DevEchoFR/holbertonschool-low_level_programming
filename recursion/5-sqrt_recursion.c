9329365e4d7d10da793f8bb682846d3b53d2ecef
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 14:57:02 +0000

    feat(math): add _sqrt_recursion + helper (natural sqrt).

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,27 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * sqrt_helper - helper function to find the square root recursively
- * @n: the number to find the square root of
- * @guess: the current guess for the square root
+ * sqrt_recursion1 - helper function to find the natural square root
+ * @n: number to find the square root of
+ * @i: iterator
  *
- * Return: the square root of n, or -1 if n has no natural square root
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ * square root
  */
 
-int sqrt_helper(int n, int guess)
+int sqrt_helper(int n, int i)
 {
-	if (guess * guess == n)
-		return (guess);
-	if (guess * guess > n)
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
 		return (-1);
-	return (sqrt_helper(n, guess + 1));
+	return (sqrt_helper(n, i + 1));
 }
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
+ * @n: number to find the square root of
  *
- * Return: the square root of n, or -1 if n has no natural square root
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ * square root
  */
 
 int _sqrt_recursion(int n)
