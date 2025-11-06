62018b7211f52b6266f9513619035d4f18622a05
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 14:09:30 +0000

    Testing

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,6 +8,22 @@
  * Return: the square root of n, or -1 if n has no natural square root
  */
 
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the square root of n, or -1 if n has no natural square root
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
