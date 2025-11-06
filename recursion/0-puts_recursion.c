d59e6cc2606a4bd00f65cf30307b71cfec556142
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 12:42:50 +0000

    Testing

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,12 +11,20 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		if (s[i] == '\n')
+		{
+			_putchar('\n');
+			return;
+		}
+		break;
+	}
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
