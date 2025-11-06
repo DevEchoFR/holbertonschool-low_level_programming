662cbbc70ee1b767c0ebdef0dac83ffef9648b4d
Vesna David ROSET <snaboy18@gmail.com>
2025-11-06 13:10:01 +0000

    Testing

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,8 +2,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	char first_char = *s;
+	if (first_char == '\0')
 		return;
 	_print_rev_recursion(s + 1);
-	_putchar("0"[*s]);
+	_putchar(first_char);
 }
