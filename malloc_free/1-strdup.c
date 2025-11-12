781195fac993dc0501be09bc6d940939d371023a
Vesna David ROSET <snaboy18@gmail.com>
2025-11-12 08:03:52 +0000

    feat(strings): add _strdup (heap duplicate).

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string by allocating new memory.
+ * @str: the string to duplicate.
+ *
+ * Return: pointer to the duplicated string, or NULL if it fails.
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	dup = (char *)malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i];
+	}
+	dup[len] = '\0';
+
+	return (dup);
+}
