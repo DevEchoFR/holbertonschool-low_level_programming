f04616f07812481c3ab2f2942df698c950707301
Vesna David ROSET <snaboy18@gmail.com>
2025-11-12 08:10:35 +0000

    feat(memory): add free_grid (free 2D int grid).

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D array of integers.
+ * @grid: pointer to the 2D array.
+ * @height: height of the grid.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
