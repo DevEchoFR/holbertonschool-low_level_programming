db5c4869ca07705b2e148f2f3d124d27dc77b1f5
Vesna David ROSET <snaboy18@gmail.com>
2025-11-17 15:29:45 +0000

    feat(variadic): add sum_them_all (va-sum).

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of parameters
+ *
+ * Return: sum of parameters, or 0 if n is 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	return (0);
+}
