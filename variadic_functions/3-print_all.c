e097b1dc4172e4e9c70f7710d27e593082b37b4f
Vesna David ROSET <snaboy18@gmail.com>
2025-11-17 15:49:40 +0000

    feat(variadic): add print_strings (sep + newline).

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything based on format
+ * @format: list of types of the arguments
+ *
+ * c: char, i: integer, f: float, s: char *
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list ap;
+unsigned int i = 0;
+char *sep = "";
+char *s;
+va_start(ap, format);
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, (char)va_arg(ap, int));
+sep = ", ";
+break;
+case 'i':
+printf("%s%d", sep, va_arg(ap, int));
+sep = ", ";
+break;
+case 'f':
+printf("%s%f", sep, va_arg(ap, double));
+sep = ", ";
+break;
+case 's':
+s = va_arg(ap, char *);
+if (s == NULL)
+	s = "(nil)";
+printf("%s%s", sep, s);
+sep = ", ";
+break;
+}
+i++;
+}
+va_end(ap);
+printf("\n");
+}
