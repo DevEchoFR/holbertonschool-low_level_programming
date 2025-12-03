92588a11eee5ba92e5cea4ec43c016ef7b7fd35e
Vesna David ROSET <snaboy18@gmail.com>
2025-12-03 18:26:21 +0000

    feat(lists): add list_len (count nodes)

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,42 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
- *
- * Return: number of nodes
- */
-size_t print_list(const list_t *h)
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to the head of the list
+* Return: number of nodes in the list
+*/
+
+size_t list_len(const list_t *h)
 {
-    const list_t *current;
-    size_t count;
+	size_t count = 0;
+	const list_t *node = h;
 
-    /* 1. Initialize current and count */
-    current = h;
-    count = 0;
+	/* Loop through the list */
+	while (node != NULL)
+	{
+		count++;                /* Increase the counter (count) */
+		node = node->next;      /* Move to the next node in the list */
+	}
 
-    /* 2. Loop through the list */
-    while (current != NULL)
-    {
-        /* 3. Check if current->str is NULL */
-        if (current->str == NULL)
-        {
-            /* print formats "[0] (nil)\n" */
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            /* print "[len] str\n" */
-            printf("[%u] %s\n", current->len, current->str);
-        }
-
-        /* 4. Increase count */
-        count++;
-
-        /* 5. Move to next node */
-        current = current->next;
-    }
-
-    /* 6. Return number of nodes */
-    return (count);
+	return (count);
 }
