5cfb6522a00196471b3b68ec8b572d0023ef239f
Vesna David ROSET <snaboy18@gmail.com>
2025-12-03 17:57:10 +0000

    Fixing struct list_t

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 /**
- * struct list_s - singly linked list
+ * struct list_t - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -11,14 +11,12 @@
  */
 
 typedef struct list_t
-/**@list_t */
 {
 char *str;
 unsigned int len;
 struct list_t *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
